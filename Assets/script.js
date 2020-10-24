4acabfc09ed531113e1fc550e00883e284b47184
Eduardo Coello <jeck24@hotmail.com>
2020-10-24 00:27:01 -0500

    finally working

--- Assets/script.js
+++ Assets/script.js
@@ -5,28 +5,51 @@ var option1Btn = document.querySelector("#option1");
 var option2Btn = document.querySelector("#option2");
 var option3Btn = document.querySelector("#option3");
 var option4Btn = document.querySelector("#option4");
-var counterDisplay = document.querySelector(".counter");
-var scoreDisplay = document.querySelector(".score");
-
-
-var count=0;
-var timer=0;
-var score=0;
-var questionTime=59;
+var counterDisplay = document.querySelector("#counterDisplay");
+var scoreDisplay = document.querySelector("#score");
+var resultMessage = document.querySelector(".resultMessage")
 
 var questions = [
-    { q: "Which are the only two places in the world where you can't buy Coca-Cola?", choiceA: "North Korea & Cuba", choiceB:"Mexico & Belize", choiceC: "Saudi Arabia & Iran", choiceD: "China & Myanmar", correct: "A"},
-    { q: "Which percentage of the entire world's population can fit inside LA?", choiceA: "100%", choiceB:"80%", choiceC: "60%", choiceD:"40%", correct:"A"},
-    { q: "When has the number of twins reached the maximum level in history?", choiceA: "Now", choiceB:"20 years ago", choiceC:"100 years ago", choiceD:"1000 years ago", correct:"A"},
-    { q: "What is the worst that can happen to you by eating the hottest chili?", choiceA: "Kill you", choiceB: "Bad diarrhea", choiceC: "Faint", choiceD: "Nothing"},
-    { q: "What is the country with the highest number of tourists?", choiceA: "France", choiceB: "US", choiceC: "China", choiceD: "Australia", correct:"A"},
-    { q: "What is the size in number of soccer fields of the world's most densely populated island?", choiceA: "2", choiceB:"10", choiceC: "1", choiceD: "<1", correct:"A"},
-    { q: "After what animal, were The Canary Islands named after?", choiceA: "Dog", choiceB:"Bird", choiceC: "Horse", choiceD: "Lion", correct: "A"},
-    { q: "Which country has the shortest people in the world?", choiceA: "Indonesia", choiceB: "Phillippines", choiceC: "Congo", choiceD: "Bolivia", correct: "A"},
-    { q: "Where is the quietest room in the world?", choiceA: "Microsoft's headquarters", choiceB: "Lamma Temple", choiceC: "Tibet", choiceD: "CDMX", correct:"A"}
+    { q: "Which are the only two places in the world where you can't buy Coca-Cola?", choiceA: "North Korea & Cuba", choiceB:"Mexico & Belize", choiceC: "Saudi Arabia & Iran", choiceD: "China & Myanmar", correct: "North Korea & Cuba"},
+    { q: "Which percentage of the entire world's population can fit inside LA?", choiceA: "100%", choiceB:"80%", choiceC: "60%", choiceD:"40%", correct:"100%"},
+    { q: "When has the number of twins reached the maximum level in history?", choiceA: "Now", choiceB:"20 years ago", choiceC:"100 years ago", choiceD:"1000 years ago", correct:"Now"},
+    { q: "What is the worst that can happen to you by eating the hottest chili?", choiceA: "Kill you", choiceB: "Bad diarrhea", choiceC: "Faint", choiceD: "Nothing", correct: "Kill you"},
+    { q: "What is the country with the highest number of tourists?", choiceA: "France", choiceB: "US", choiceC: "China", choiceD: "Australia", correct:"France"},
+    { q: "What is the size in number of soccer fields of the world's most densely populated island?", choiceA: "2", choiceB:"10", choiceC: "1", choiceD: "<1", correct:"2"},
+    { q: "After what animal, were The Canary Islands named after?", choiceA: "Dog", choiceB:"Bird", choiceC: "Horse", choiceD: "Lion", correct: "Dog"},
+    { q: "Which country has the shortest people in the world?", choiceA: "Indonesia", choiceB: "Phillippines", choiceC: "Congo", choiceD: "Bolivia", correct: "Indonesia"},
+    { q: "Where is the quietest room in the world?", choiceA: "Microsoft's headquarters", choiceB: "Lamma Temple", choiceC: "Tibet", choiceD: "CDMX", correct:"Microsoft's headquarters"}
   ];
 
 var lastQuestion = questions.length-1;
+var count=0;
+var timer=0;
+var score=0;
+var questionTime=15;
+var speed = setInterval(counter,1000);
+
+option1Btn.addEventListener("click", function(){
+    answer = option1Btn.textContent;
+    checkAnswer(answer);
+
+});
+
+option2Btn.addEventListener("click",function(){
+    answer = option2Btn.textContent;
+    checkAnswer(answer);
+
+});
+
+option3Btn.addEventListener("click", function(){
+    answer = option3Btn.textContent;
+    checkAnswer(answer);
+});
+
+option4Btn.addEventListener("click",function(){
+    answer = option4Btn.textContent;
+    checkAnswer(answer);
+
+});
 
 function displayQuestion(){
     var question = questions[count];
@@ -35,8 +58,6 @@ function displayQuestion(){
     option2Btn.textContent=question.choiceB;
     option3Btn.textContent=question.choiceC;
     option4Btn.textContent=question.choiceD;
-    console.log(questionText.textContent);
-    count++;
 }
 
 function counter(){
@@ -52,9 +73,14 @@ function counter(){
         }
         else{
             displayScore();
+            saveHighscore();
         }
     }
 
+}
+
+function saveHighscore(){
+    
 }
 
 function displayScore(){
@@ -65,9 +91,13 @@ function checkAnswer(answer){
 
     if(questions[count].correct==answer){
         score++;
+        displayScore();
+        resultMessage.textContent="Correct";
     }
     else{
         score--;
+        displayScore();
+        resultMessage.textContent="Wrong";
     }
 
     if(count<lastQuestion){
@@ -75,58 +105,21 @@ function checkAnswer(answer){
         displayQuestion();
     }
     else{
-        clearInterval(timer);
-        alert("Your score is: "+score);
+        clearInterval(speed);
+        displayScore();
     }
-
-
-    option1Btn.addEventListener("click", function(){
-        answer = option1Btn.textContent;
-        if (answer === questions[count].correct){
-            score ++;
-        }
-
-    });
-
-    option2Btn.addEventListener("click",function(){
-        answer = option2Btn.textContent;
-        if (answer === questions[count].correct){
-            score ++;
-            count++;
-        }
-
-    });
-
-    option3Btn.addEventListener("click", function(){
-        answer = option3Btn.textContent;
-        if (answer === questions[count].correct){
-            score ++;
-            count++;
-        }
-
-    });
-
-    option4Btn.addEventListener("click",function(){
-        answer = option4Btn.textContent;
-        if (answer === questions[count].correct){
-            score ++;
-            count++;
-        }
-
-    });
+    
 }
 
 function askQuestions(){
 
 
-    counter();
-    timer=setInterval(counter,1000);
     displayQuestion();
-    checkAnswer();
+    counter();
+    speed=setInterval(counter,1000);
           
-    
-    console.log(score);
 }
+
 
 startButton.addEventListener("click", askQuestions);
 
